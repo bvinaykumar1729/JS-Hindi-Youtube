37e599bdfa699a731db196ea4e93693d6b3206b6
VinayKumar <122444695+bvinaykumar1729@users.noreply.github.com>
2024-04-02 13:54:26 +0000

    Comparision of datatypes

---
+++ .devcontainer/basics/04_comparisionOfDatatypes.js
@@ -0,0 +1,25 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2 != 1);
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+
+// console.log(null>=0);
+/*The reason is that an equality check == and comparisons><>=
+<= work differeently.
+Comparision convert null to a number treating it as 0.
+Thatswhy(3)null>=0 is true null>0 is false */
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//strict check === also checking datatype
+
+console.log("2"===2);
