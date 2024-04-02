55daf533a917dbc1a4228f34890d5184a355e570
VinayKumar <122444695+bvinaykumar1729@users.noreply.github.com>
2024-04-02 14:20:56 +0000

    Datatypes Summary

---
+++ .devcontainer/basics/datatypes-summary.js
@@ -0,0 +1,52 @@
+//primitives
+
+/*7 types :
+1.String
+2.Number
+3.Boolean
+4.null
+5.undefined
+6.Symbol
+7.BigInt
+*/
+
+
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+console.log(id===anotherId);
+
+const bigNumber=34567890123456789n
+
+
+//Reference (Non primitive)
+
+/*3 types
+1.Array
+2.Objects
+3.Functions
+*/
+
+const heros =["shaktiman","nagaraj","doga"];
+let myObj ={
+    name:"vinay",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello Wwrld");
+}
+
+//console.log(typeof outsideTemp );
+console.log(typeof myFunction);
+//function returns function object
+
+//console.log(typeof anotherId);
