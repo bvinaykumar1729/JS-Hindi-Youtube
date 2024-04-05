0fb2395483b3cea1c22328b4795c7b877929b1cc
VinayKumar <122444695+bvinaykumar1729@users.noreply.github.com>
2024-04-05 18:10:31 +0000

    Datatypes in js

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs, not browser
+
+console.log(3+
+    3)//code readability shoud be high
+
+    console.log("VinayKumar")
+
+    let name="Vinay";
+    let age=22;
+    let isLoggedIn=false;
+
+    //number => 2 to power 53
+    //bigint
+    //string=>""
+    //boolean =>true/false
+    //null => standalone value
+    //underfined =>
+    //symbol =>uniqueness
+
+    //object
+
+    console.log(typeof null)//object;
+    console.log(typeof undefined)//undefined
