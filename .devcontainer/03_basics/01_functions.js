309b346828cbe0d87ab3a82d51da6b9b6fb58d28
VinayKumar <122444695+bvinaykumar1729@users.noreply.github.com>
2024-04-11 13:34:54 +0000

    Functions and parameters

---
+++ .devcontainer/03_basics/01_functions.js
@@ -0,0 +1,38 @@
+
+function sayMyName(){
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+// sayMyName()
+
+// function addTwoNumbers(number1,number2){
+//     console.log(number1+number2);
+// }
+// addTwoNumbers(3,"4")
+// addTwoNumbers(3,"a")
+// addTwoNumbers(3, null)//3
+
+function addTwoNumbers(number1,number2){
+//    let result = number1+number2
+//    return result
+  return number1+number2
+
+}
+// const result = addTwoNumbers(3,5);
+// console.log("Result: ",result);
+
+function loginUserMessage(username = "sam"){
+  if(!username){
+    console.log("Please eenter a username");
+    return
+  }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("hitesh"));
+console.log(loginUserMessage());
