2f1550276a8916496902648953c06f4a11133159
VinayKumar <122444695+bvinaykumar1729@users.noreply.github.com>
2024-04-26 01:59:42 +0000

    if -else condition statements

---
+++ .devcontainer/04_control_flow/one.js
@@ -0,0 +1,48 @@
+// if
+const isUserloggedIn = true
+const temperature = 41
+// if(temperature === 40){
+// console.log("less than 50")
+// }else {
+//     console.log("temperature is greater than 50");
+// }
+
+// console.log("Execute");
+
+// <,>,<=,>=,==, !=, ===
+
+// const score = 200
+// if(score > 100) {
+//     const power ="fly"
+//     console.log(`User power:${power}`);
+// }
+
+// console.log(`User power:${power}`);
+
+
+const balance = 1000;
+
+// if(balance>500) console.log("test"),console.log("passed");
+
+// if (balance < 500) {
+//     console.log("less than 500");   
+// } else if (balance < 750) {
+//     console.log("less than 750");
+// }else if(balance < 900){
+//     console.log("less than 900");
+// }else{
+//     console.log("less thsn 1200");
+// }
+
+const userLoggedIn =ture;
+const debitCard = ture;
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if(userLoggedIn && debitCard && 2==2) {
+    console.log("Allow to buy course");
+}
+
+if(loggedInFromEmail || loggedInFromGoogle) {
+    console.log("User logged in");
+}
