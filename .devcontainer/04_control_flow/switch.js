915bf8b664f86c931f8af6365db6c79250f66b1d
VinayKumar <122444695+bvinaykumar1729@users.noreply.github.com>
2024-04-26 02:00:17 +0000

    switch case

---
+++ .devcontainer/04_control_flow/switch.js
@@ -0,0 +1,34 @@
+// switch (key) {
+//     case value :
+//         break;
+//     default:
+//         break;
+// }
+
+const month = "march"
+switch(month){
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "march":
+        console.log("March");
+        break;
+    case "april":
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    default:
+        console.log("default case match"); 
+        break;   
+}
